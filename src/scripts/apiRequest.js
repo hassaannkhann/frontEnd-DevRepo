0f23a2e19b2b9a313235235ddf09e8e3893ba9e2
Hassaan Khan <kahnamite10@gmail.com>
2021-12-01 15:04:38 +0500

    Used online API service to display data on the website (See more products excluded)

---
+++ src/scripts/apiRequest.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function($){
+    $.get("https://api.npoint.io/f6112511583fb9acf856", function(data){
+        // Display the returned data in browser
+        $('#featured-image-877 .product-single__photo').attr('src', data.featured_ImgUrl);
+        $('#id_D').html(data.title);
+        $('#ProductPrice').html(data.currency + ' ' + data.price);
+        $('#descText').html(data.description);
+        var items = [];
+        $.each( data.description_features, function( key, val ) {
+            $( "<span>" + val + "</span>" ).appendTo( $('.description-text__main'));
+        });
+        $.each( data.available_sizes, function( key, val ) {
+            $( "<span>" + val + "</span>" ).appendTo( $('.product--size'));
+        });
+        $.each( data.colors, function( key, val ) {
+            $( '<label id="'+ val +'" class="color--'+ val +'"><input type="radio" name="radio"> </label>' ).appendTo( $('.product-single__variation'));
+        });
+    })
+    .fail(function() {
+        console.log( "error" );
+    })
+    .always(function() {
+        console.log( "complete" );
+    });
+})
